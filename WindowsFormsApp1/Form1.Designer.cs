1e721fa023767f15331a48ecb7906682b92b71e4
Your Name <you@example.com>
2019-04-08 18:04:45 +0100

    initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(6, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "WAITING FOR GAME";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 5000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 500;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Damage done in zone";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DPS (average over 5 sec.)";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.IndianRed;
+            this.label5.Location = new System.Drawing.Point(-2, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 36);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "0.0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label6.Location = new System.Drawing.Point(-3, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(248, 34);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 103);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MHW DPS Meter Alpha Ver. 0.15";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int PROCESS_WM_READ = 0x0010;
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool ReadProcessMemory(int hProcess,
+        Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        private Process process;
+        private Process[] processes;
+        private IntPtr processHandle;
+
+        private long baseAddress = -1;
+
+        private long[] offsets = { 0x047E6928, 0x1B0, 0x290, 0x180, 0xE8, 0x44 };
+
+        private bool initializeCheck = false;
+
+        private long startingPoint = -1;
+        private long startingPointCheck = -1;
+
+        private int[] hitDmg = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private float[] isHitting = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        private int[] isHittingCheck = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        private int zoneDmg = 0;
+        private int zoneDmgCheck = 0;
+        private float dps = 0.0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost=true;
+            timer4.Start();
+            int temp = 3;
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            processes = Process.GetProcessesByName("MONSTERHUNTERWORLD");
+            if ((processes.Length == 0) && (!initializeCheck))
+            {
+
+            }
+            else if ((processes.Length != 0) && (!initializeCheck))
+            {
+                label2.Hide();
+                label3.Show();
+                label4.Show();
+                label5.Show();
+                label6.Show();
+                process = processes[0];
+                processHandle = OpenProcess(this.PROCESS_WM_READ, false, process.Id);
+
+                foreach (ProcessModule M in process.Modules)
+                {
+                    if (M.ModuleName.ToLower() == "MonsterHunterWorld.exe".ToLower())
+                    {
+                        baseAddress = (long)M.BaseAddress;
+                    }
+                }
+
+                read64bitPointerList();
+                startingPointCheck = startingPoint;
+
+                initializeDmg();
+
+                this.timer1.Start();
+                this.timer2.Start();
+                this.timer3.Start();
+                initializeCheck = true;
+            }
+            else if((processes.Length==0) && initializeCheck)
+            {
+                Application.Exit();
+            }
+            
+        }
+        private void initializeDmg()
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                hitDmg[i] = read4byteAddress(startingPoint + (i * 0x90));
+            }
+            for (int i = 0; i < 18; i++)
+            {
+                isHitting[i] = readFloatAddress(startingPoint + (i * 0x90) - 0x4);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            initializeDmg();
+
+            for (int i = 0; i < 18; i++)
+            {
+                if ((Math.Round(isHitting[i], 1) != 0.0) && (isHittingCheck[i] == 0))
+                {
+                    zoneDmg = zoneDmg + hitDmg[i];
+                    isHittingCheck[i] = 1;
+                }
+                if ((Math.Round(isHitting[i], 1) == 0.0) && (isHittingCheck[i] == 1))
+                {
+                    isHittingCheck[i] = 0;
+                }
+            }
+
+            //debugPrintDmg();
+            printDmg();
+        }
+
+        void printDmg()
+        {
+            label6.Text = zoneDmg.ToString();
+            label5.Text = string.Format("{0:F1}", dps);
+        }
+
+        private void debugPrintDmg()
+        {
+            //label1.Text = string.Format("{0:F1}", isHitting[0]);
+            //label1.Text = isHitting[0].ToString("0.####################################################################################");
+            label1.Text = (Math.Round(isHitting[0], 1) != 0.0).ToString();
+            //label1.Text = Math.Round(isHitting[0], 1).ToString();
+            label5.Hide();
+            label1.Show();
+        }
+
+        private void read()
+        {  
+        }
+
+        private int readByteAddress(long address)
+        {
+            byte[] buffer = new byte[1];
+            int bytesRead = new int();
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+
+            return buffer[0];
+        }
+
+        private int read4byteAddress(long address)
+        {
+            byte[] buffer = new byte[4];
+            int bytesRead = new int();
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+
+            return BitConverter.ToInt32(buffer, 0);
+        }
+
+        private float readFloatAddress(long address)
+        {
+            byte[] buffer = new byte[4];
+            int bytesRead = new int();
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+
+            return BitConverter.ToSingle(buffer, 0);
+        }
+
+        private long read8byteAddress(long address)
+        {
+            byte[] buffer = new byte[8];
+            int bytesRead = new int();
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+               
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        private void read64bitPointerList()
+        {
+            long result = baseAddress;
+            for (int i = 0; i < offsets.Length; i++)
+            {
+                if (i == (offsets.Length - 1))
+                {
+                    result = result + offsets[i];
+                }
+                else
+                {
+                    result = read8byteAddress(result + offsets[i]);
+                }
+            }
+            startingPoint = result;
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            read64bitPointerList();
+            if (startingPoint != startingPointCheck)
+            {
+                this.timer1.Stop();
+                this.timer3.Stop();
+                //reset
+                zoneDmg = 0;
+                dps = 0.0f;
+
+                startingPointCheck = startingPoint;
+                this.timer1.Start();
+                this.timer3.Start();
+            }
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            dps = ((float)zoneDmg - (float)zoneDmgCheck) / 5;
+            zoneDmgCheck = zoneDmg;
+            if(dps<0)
+            {
+                dps = 0.0f;
+            }
+        }
+
+        
+    }
+}
